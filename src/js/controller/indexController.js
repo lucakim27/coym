747cf6c083268c14c76a83ed4fd7577b9f4f6f12
lucakim27 <nikolaikim27@gmail.com>
2022-01-23 22:12:52 +0800

    Update controller for register

--- src/js/controller/indexController.js
+++ src/js/controller/indexController.js
@@ -1,25 +1,28 @@
-var app = angular.module('indexHeader', [])
-    app.controller('indexHeaderController', function($scope) {
-        $scope.login = 0
+var app = angular.module('index', [])
+    app.controller('indexController', function($scope) {
+        // loginStatus, 0: logged in. 1: not logged in.
+        $scope.loginStatus = 0
         $scope.firstName = "Heesu"
         $scope.lastName = "Kim"
 
-        $scope.registerEmailInput = ""
-        $scope.registerPasswordInput = ""
-        $scope.registerPasswordAgainInput = ""
+        $scope.clickRegister = function($id, $pwd, $pwdAgain) {
 
-        $scope.clickRegister = function($email, $pwd, $pwdAgain) {
-            if ($email === undefined) {
-                alert("Your email is invalid, please follow the email format.")
-                return 0;
+            console.log($id, $pwd, $pwdAgain)
+
+            if ($id === undefined || $pwd === undefined || $pwdAgain === undefined) {
+                alert("Please fill in the blanks.")
+            } else if ($id.length < 5 || $pwd.length < 5 || $pwdAgain.length < 5) {
+                alert("The length of ID or Password should be at least more than 5 characters.")
             } else if ($pwd !== $pwdAgain) {
                 alert("Your passwords aren't matching.")
-                return 0;
             } else {
-                // use Angular Modal popup
+                alert("Your account has just registered.")
+                document.location.href = 'http://localhost:3000/login'
             }
-
-            
-
         }
+
+        $scope.clickLoginBtn = function() {
+            document.location.href = 'http://localhost:3000/login'
+        }
+        
 })
