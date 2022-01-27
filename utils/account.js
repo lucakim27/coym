a9bc233e431028b537d19dd4c4a18d458a55dd69
lucakim27 <nikolaikim27@gmail.com>
2022-01-27 23:17:13 +0800

    Create account utility

---
+++ utils/account.js
@@ -0,0 +1,14 @@
+const accounts = []
+
+function getAccounts() {
+    return account
+}
+
+function addAccount(newAcc) {
+    accounts.push(newAcc)
+}
+
+module.exports = {
+    getAccounts,
+    addAccount
+}
