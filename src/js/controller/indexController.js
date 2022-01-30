0d380981556ec9cc90274b9661645c83d4c30e02
lucakim27 <nikolaikim27@gmail.com>
2022-01-30 22:48:06 +0800

    Delete unused functions in Angular Controller

--- src/js/controller/indexController.js
+++ src/js/controller/indexController.js
@@ -5,24 +5,7 @@ var app = angular.module('index', [])
         $scope.firstName = "Heesu"
         $scope.lastName = "Kim"
 
-        $scope.clickRegister = function($id, $pwd, $pwdAgain) {
-
-            console.log($id, $pwd, $pwdAgain)
-
-            if ($id === undefined || $pwd === undefined || $pwdAgain === undefined) {
-                alert("Please fill in the blanks.")
-            } else if ($id.length < 5 || $pwd.length < 5 || $pwdAgain.length < 5) {
-                alert("The length of ID or Password should be at least more than 5 characters.")
-            } else if ($pwd !== $pwdAgain) {
-                alert("Your passwords aren't matching.")
-            } else {
-                alert("Your account has just registered.")
-                document.location.href = 'http://localhost:3000/login'
-            }
-        }
-
         $scope.clickLoginBtn = function() {
             document.location.href = 'http://localhost:3000/login'
         }
-        
 })
