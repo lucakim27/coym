16bc0bc10c2990fe3d8a29f5c263120136755ea8
Heesu Kim <nikolaikim27@gmail.com>
2022-03-19 17:16:46 +0800

    Fix chart table when there's no data

--- src/js/chart.js
+++ src/js/chart.js
@@ -13,15 +13,22 @@ socket.on('getMostViewed', (value) => {
         socket.emit('getId', socketid)
     }
 
-    const keys = Object.keys(value)
-
-    keys.forEach((key, index) => {
-        var row = table.insertRow(index)
+    if (JSON.stringify(value) == '{}') {
+        var row = table.insertRow(0)
         var cell = row.insertCell(0)
         var p = document.createElement('p')
-        p.innerHTML = `${key}: ${value[key]}<hr>`
+        p.innerHTML = `There's no data at the moment.`
         cell.appendChild(p)
-    });
+    } else {
+        const keys = Object.keys(value)
+        keys.forEach((key, index) => {
+            var row = table.insertRow(index)
+            var cell = row.insertCell(0)
+            var p = document.createElement('p')
+            p.innerHTML = `${key}: ${value[key]}<hr>`
+            cell.appendChild(p)
+        })
+    }
 
 })
 
