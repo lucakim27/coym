550c6ec92ef3724362148a81cfcf00851c8b65ee
lucakim27 <nikolaikim27@gmail.com>
2022-04-04 22:39:55 +0800

    Convert loop to recursive function with jQuery

--- src/js/home.js
+++ src/js/home.js
@@ -1,55 +1,78 @@
 const socket = io()
 const occupationArray = []
 var socketid = ''
+var occupationTable = document.getElementById("occupationTable")
+var index = 0
 
 socket.on('userEnter', (array) => {
-    for (var i = 0; i < array.length; i++) {
-        occupationArray.push(array[i])
-    }
-    
-    for (var i = 0; i < occupationArray.length; i++) {
-        var row = document.getElementById("occupationTable").insertRow(i)
-        var cell = row.insertCell(0)
-        cell.insertAdjacentHTML('beforeend', `
-            <a href='http://localhost:3000/comment?occupation=${occupationArray[i][0]}' onclick="countForCharts('${occupationArray[i][0]}')">
-                ${occupationArray[i][0]}
-            </a><hr>`
-        )
-    }
-
-    if (document.getElementById('userId').innerText == 'anonymous') {
-        document.getElementById('logoutBtn').style.display = 'none'
-        document.getElementById('loginBtn').style.display = 'block'
+    pushOccupationArray(array)
+    listOccupations()
+    if ($("#userId").text() == 'anonymous') {
+        $("#logoutBtn").css('display', 'block')
+        $("#logoutBtn").css('display', 'none')
     } else {
-        document.getElementById('loginBtn').style.display = 'none'
-        document.getElementById('logoutBtn').style.display = 'block'
+        $("#logoutBtn").css('display', 'block')
+        $("#loginBtn").css('display', 'none')
     }
-    
-    socket.emit('addOnlineUser', document.getElementById('userId').innerText)
+    socket.emit('addOnlineUser', $("#userId").text())
 })
 
 socket.on('updatedComment', (array) => {
     occupationArray = array
 })
 
-function countForCharts(occupationName) {
-    socket.emit('countUpMostViewed', occupationName)
+const pushOccupationArray = function(array) {
+    if (index < array.length) {
+        occupationArray.push(array[index])
+        index++
+        pushOccupationArray(array)
+    } else {
+        index = 0
+    }
 }
 
-function searchOccupationBtn() {
-    var table = document.getElementById("occupationTable")
-    var countRow = 0
-    table.innerHTML = ""
-    for (var i = 0; i < occupationArray.length; i++) {
-        if (occupationArray[i][0].toLowerCase().includes(document.getElementById('userInput').value.toLowerCase()) == true) {
-            var row = table.insertRow(countRow)
-            var cell = row.insertCell(0)
-            var a = document.createElement('a')
-            a.href = `http://localhost:3000/comment?occupation=${occupationArray[i][0]}`
-            a.innerHTML = occupationArray[i][0]
-            cell.appendChild(a)
-            cell.appendChild(document.createElement('hr'))
-            countRow++
-        }
+const listOccupations = function() {
+    if (index < occupationArray.length) {
+        var row = occupationTable.insertRow(index)
+        var cell = row.insertCell(0)
+        cell.insertAdjacentHTML('beforeend', `
+            <a 
+                href='http://localhost:3000/comment?occupation=${occupationArray[index][0]}' 
+                onclick="countForCharts('${occupationArray[index][0]}')"
+            >${occupationArray[index][0]}
+            </a><hr>`
+        )
+        index++
+        listOccupations()
+    } else {
+        index = 0
     }
+}
+
+// var rowNum = 0
+// const searchOccupations = function() {
+//     alert('index: ' + index)
+//     alert('rowNum: ' + rowNum)
+//     occupationTable.innerHTML = ''
+//     if (index < occupationArray.length) {
+//         alert(occupationArray[index][0].toLowerCase().includes(document.getElementById('userInput').value.toLowerCase()))
+//         if (occupationArray[index][0].toLowerCase().includes(document.getElementById('userInput').value.toLowerCase()) == true) {
+//             var row = occupationTable.insertRow(rowNum)
+//             var cell = row.insertCell(0)
+//             var a = document.createElement('a')
+//             a.href = `http://localhost:3000/comment?occupation=${occupationArray[index][0]}`
+//             a.innerHTML = occupationArray[index][0]
+//             cell.appendChild(a)
+//             cell.appendChild(document.createElement('hr'))
+//             rowNum++
+//         }
+//         index++
+//         searchOccupations()
+//     } else {
+//         index = 0
+//     }
+// }
+
+const countForCharts = function (occupationName) {
+    socket.emit('countUpMostViewed', occupationName)
 }
