1f43a3409dd87be042293f52c157587ae4818120
lucakim27 <nikolaikim27@gmail.com>
2022-06-20 01:24:49 +0900

    Conversion to Typescript 3

--- src/ts/sub/menuBar.ts
+++
@@ -1,9 +0,0 @@
-let navigationBar: any = document.getElementById('mySidenav')
-
-const sidebarOpen = function() {
-  navigationBar.style.width = "250px"
-}
-
-const sidebarClose = function() {
-  navigationBar.style.width = "0"
-}
