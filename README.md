28f9e467aaca762f8fb20fbefc6274cd475a2f3a
lucakim27 <nikolaikim27@gmail.com>
2022-07-01 14:35:36 +0900

    Redesign about page

--- README.md
+++ README.md
@@ -4,24 +4,17 @@ Comment-On-Your-Occupation - COYO
 # Objective
 Many students in the world are facing difficulties in figuring out what occupation they want to have in the future but there aren't many places where you can get some ideas from. so I made a decision to make a website that helps them out through the comments from people who actually have the occupation. so they wouldn't be wasting of the time to figure it out what it's like to be. they would make a better decision when they're deciding the university course as well.
 
-# Pages
-- Index: A page where you see it when you access the website for the first time.
-- Home: A page where you're directed to from the click of button 'start' on the index page.
-- Online: A page where you see the online logged in users and your friends list.
-- Chat(In progress): A page where you chat with your friends.
-- Chart: A page where you see how many times they've been accessing to the the page and commenting on the occupations.
-- About: A page where it contains an information about the website.
-- Contact: A function that opens the e-mail program and has my e-mail address.
-- Each occupations sub page: A page where you comment and like on the comments.
-
 # Libraries
 - jquery
 - ejs template
 - socket.io
 
-# Framework
+# Frameworks
 - node.js
 - express
+
+# Database
+- mysql2
 
 # How to hop on the project
 You're welcome to make as many pull requests as you want to as well as the issues.
--- controllers/about.js
+++ controllers/about.js
@@ -6,3 +6,88 @@ try {
 } catch (error) {
     console.error(error)
 }
+
+var left = 1
+var right = 1
+
+$('#incrementLeft').click(function () {
+    if (left === 3) {
+        left = 1
+    } else {
+        left++
+    }
+    showAndHide(left, 'left')
+})
+
+$('#incrementRight').click(function () {
+    if (right === 3) {
+        right = 1
+    } else {
+        right++
+    }
+    showAndHide(right, 'right')
+})
+
+$('#decrementLeft').click(function () {
+    if (left === 1) {
+        left = 3
+    } else {
+        left--
+    }
+    showAndHide(left, 'left')
+})
+
+$('#decrementRight').click(function () {
+    if (right === 1) {
+        right = 3
+    } else {
+        right--
+    }
+    showAndHide(right, 'right')
+})
+
+const showAndHide = function (number, direction) {
+    if (direction === 'left') {
+        $("#onlineHead").hide()
+        $("#onlineParagraph").hide()
+        $("#commentHead").hide()
+        $("#commentParagraph").hide()
+        $("#homeHead").hide()
+        $("#homeParagraph").hide()
+        switch (number) {
+            case 1:
+                $("#homeHead").show()
+                $("#homeParagraph").show()
+                break;
+            case 2:
+                $("#onlineHead").show()
+                $("#onlineParagraph").show()
+                break;
+            case 3:
+                $("#commentHead").show()
+                $("#commentParagraph").show()
+                break;
+        }
+    } else if (direction === 'right') {
+        $("#chartHead").hide()
+        $("#chartParagraph").hide()
+        $("#contactHead").hide()
+        $("#contactParagraph").hide()
+        $("#chatHead").hide()
+        $("#chatParagraph").hide()
+        switch (number) {
+            case 1:
+                $("#chatHead").show()
+                $("#chatParagraph").show()
+                break;
+            case 2:
+                $("#chartHead").show()
+                $("#chartParagraph").show()
+                break;
+            case 3:
+                $("#contactHead").show()
+                $("#contactParagraph").show()
+                break;
+        }
+    }
+}
