1f2168c289004901305084cccb08f07d8e43f9ee
lucakim27 <nikolaikim27@gmail.com>
2022-08-05 12:31:22 +0900

    Add dotenv for db

--- client/src/components/NavigationComponent.vue
+++ client/src/components/NavigationComponent.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="mySidenav" class="sidenav">
     <a href="javascript:void(0)" class="closebtn" v-on:click="sidebarClose()">&times;</a>
-    <a href="/">Home</a>
-    <a href="/online">Online</a>
-    <a href="/chat">Chat</a>
-    <a href="/chart">Chart</a>
-    <a onclick="window.location.href = 'mailto:nikolaikim27@google.com'">Contact</a>
+    <a id='chat' href="/chat">Chat</a>
+    <a id='chart' href="/chart">Chart</a>
+    <a id='home' href="/">Home</a>
+    <a id='online' href="/online">Online</a>
+    <a id='settings' href="/settings">Settings</a>
+    <a id='about'>About us</a>
+    <a onclick="window.location.href = 'mailto:nikolaikim27@google.com'">Contact us</a>
   </div>
 </template>
 <script>
@@ -14,6 +16,20 @@ export default {
   methods: {
     sidebarClose: function () {
       document.getElementById("mySidenav").style.width = "0"
+    }
+  },
+  mounted() {
+    if (document.getElementById(this.getTitle) !== null) {
+      document.getElementById(this.getTitle).style.color = '#f1f1f1'
+    }
+  },
+  computed: {
+    getTitle() {
+      if (new URL(window.location.href).pathname.slice(1, new URL(window.location.href).pathname.length) === '') {
+        return 'home'
+      } else {
+        return new URL(window.location.href).pathname.slice(1, new URL(window.location.href).pathname.length).toLowerCase()
+      }
     }
   }
 };
--- server/configs/db.ts
+++ server/configs/db.ts
@@ -1,7 +1,8 @@
 import mysql2 from 'mysql2'
+import 'dotenv/config'
 
 export const connection = mysql2.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "coym"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME
 })
