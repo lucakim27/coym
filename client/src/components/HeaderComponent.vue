5b21abad975125b7a860ef7294324e29b6bf615d
lucakim27 <nikolaikim27@gmail.com>
2022-08-08 23:20:32 +0900

    Fix username issue

--- client/src/components/HeaderComponent.vue
+++ client/src/components/HeaderComponent.vue
@@ -14,7 +14,7 @@
                         d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                 </svg>
                 <div id='profileDropdown' class="dropdown-content">
-                    <a>{{ this.cookies.get("user").username }}</a>
+                    <a>{{ username }}</a>
                     <a href="/setting">Setting</a>
                     <a @click='logout()' class="signOutBtn">Sign out</a>
                 </div>
@@ -45,6 +45,7 @@ export default {
     },
     data() {
         return {
+            username: '',
             loggedIn: false,
             showModal: false,
             // socket: io('http://localhost:3001', {
@@ -59,6 +60,7 @@ export default {
     beforeMount() {
         let self = this
         if (self.cookies.get('user') !== null) {
+            self.username = self.cookies.get('user')
             axios({
                 method: "GET",
                 url: "https://proxy11112321321.herokuapp.com/https://coym-api.herokuapp.com/cookieValidation",
