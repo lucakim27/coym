0194ab2b00806e583a57588409426feb993543ed
lucakim27 <nikolaikim27@gmail.com>
2022-08-13 17:36:58 +0900

    Remove header directing to home

--- client/src/components/HeaderComponent.vue
+++ client/src/components/HeaderComponent.vue
@@ -11,7 +11,7 @@
                         d="M10 11V8l5 4-5 4v-3H1v-2h9zm-7.542 4h2.124A8.003 8.003 0 0 0 20 12 8 8 0 0 0 4.582 9H2.458C3.732 4.943 7.522 2 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10c-4.478 0-8.268-2.943-9.542-7z" />
                 </g>
             </svg>
-            <h2 v-on:click="directToHome()">COYM</h2>
+            <h2>COYM</h2>
             <div class="dropdown">
                 <svg v-if="loggedIn"
                     @click.prevent="toggleDropdown" class='profileSVG' xmlns="http://www.w3.org/2000/svg" width="35"
@@ -56,9 +56,6 @@ export default {
         document.addEventListener('click', this.close)
     },
     methods: {
-        directToHome() {
-            window.location.href = '/'
-        },
         toggleDropdown() {
             this.state = !this.state
         },
