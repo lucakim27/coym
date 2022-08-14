8d44ee877af7c2870c6039c4de78f451fc8f6048
lucakim27 <nikolaikim27@gmail.com>
2022-08-14 21:06:56 +0900

    Add vue-router transition

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,13 @@
   <div>
     <HeaderComponent />
     <NavigationComponent />
-    <router-view />
+    <router-view v-slot="{ Component, route }">
+      <Transition name="fade" mode="out-in">
+        <div :key="route.name">
+          <component :is="Component"></component>
+        </div>
+      </Transition>
+    </router-view>
   </div>
 </template>
 <script>
@@ -16,3 +22,14 @@ export default {
   }
 }
 </script>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
