03b9392d1acb406bfa6c582208756ebc0ce16830
lucakim27 <nikolaikim27@gmail.com>
2022-08-23 16:48:43 +0900

    Redesign footer 2

--- client/src/components/FooterComponent.vue
+++ client/src/components/FooterComponent.vue
@@ -4,11 +4,16 @@
             <div class="footerContainer">
                 <img src="../assets/images/favicon.png" height="100">
                 <h3>Comment-On-Your-Major</h3>
-                <div class="footerLinksConatiner">
+                <div class="footerLinksConatiner" v-if="!isMobile()">
                     <a @click="render('/aboutUs')">About Us</a>
                     <a onclick="window.location.href = 'mailto:nikolaikim27@gmail.com'">Contact Us</a>
                     <a @click="render('privacyPolicy')">Privacy Policy</a>
                 </div>
+                <div class="footerLinksConatiner footerLinksMoblieContainer" v-if="isMobile()">
+                    <a @click="render('/aboutUs')">About Us</a><br>
+                    <a onclick="window.location.href = 'mailto:nikolaikim27@gmail.com'">Contact Us</a><br>
+                    <a @click="render('privacyPolicy')">Privacy Policy</a>
+                </div><br>
                 <b>2022 COYM. All Rights Reserved.</b>
             </div>
         </footer>
@@ -18,6 +23,13 @@
 export default {
     name: 'FooterComponent',
     methods: {
+        isMobile() {
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                return true
+            } else {
+                return false
+            }
+        },
         render(page) {
             this.$router.push(page)
             window.scrollTo(0,0)
