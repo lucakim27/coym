27d25014286403782468d75dee7bec9365397e7d
lucakim27 <nikolaikim27@gmail.com>
2022-08-23 16:22:15 +0900

    Redesign footer

--- client/src/components/FooterComponent.vue
+++ client/src/components/FooterComponent.vue
@@ -1,25 +1,28 @@
 <template>
-    <footer>
-        <div class="footerContainer">
-            <img src="../assets/images/favicon.png" height="100">
-            <h3>Comment-On-Your-Major</h3>
-            <div class="footerLinksConatiner">
-                <a @click="render('/aboutUs')">About Us</a>
-                <a onclick="window.location.href = 'mailto:nikolaikim27@gmail.com'">Contact Us</a>
-                <a @click="render('privacyPolicy')">Privacy Policy</a>
+    <div class="footer">
+        <footer>
+            <div class="footerContainer">
+                <img src="../assets/images/favicon.png" height="100">
+                <h3>Comment-On-Your-Major</h3>
+                <div class="footerLinksConatiner">
+                    <a @click="render('/aboutUs')">About Us</a>
+                    <a onclick="window.location.href = 'mailto:nikolaikim27@gmail.com'">Contact Us</a>
+                    <a @click="render('privacyPolicy')">Privacy Policy</a>
+                </div>
+                <b>2022 COYM. All Rights Reserved.</b>
             </div>
-            <b>2022 COYM. All Rights Reserved.</b>
-        </div>
-    </footer>
+        </footer>
+    </div>
 </template>
 <script>
-    export default {
-  name: 'FooterComponent',
-  methods: {
-    render(page) {
-      this.$router.push(page)
+export default {
+    name: 'FooterComponent',
+    methods: {
+        render(page) {
+            this.$router.push(page)
+            window.scrollTo(0,0)
+        }
     }
-  }
 }
 </script>
 <style scoped>
