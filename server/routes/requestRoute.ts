26573590164559c1620562b5442e918e62b4d711
Heesu Kim <nikolaikim27@gmail.com>
2022-08-29 02:14:39 +0900

    Separate coym-api to new repository

--- server/routes/requestRoute.ts
+++
@@ -1,9 +0,0 @@
-import { postRequest } from '../models/requestModel'
-import { pool } from '../configs/db'
-import express from 'express'
-export const router = express.Router()
-export default router
-
-router.post('/postRequest', function (req: any, res: any) {
-  postRequest(pool, res, req)
-})
