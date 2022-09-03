b94228439e565be5a3b2c759611f5ea3757c9191
Heesu Kim <nikolaikim27@gmail.com>
2022-09-03 17:24:30 +0900

    Make User details scrollable horizontally

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -4,12 +4,6 @@
             <div class='profileMostViewedContainer'>
                 <div class="profileMostViewedPages">
                     <div style="display: flex;" class="profileUsernameContainer">
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black"
-                            class="bi bi-person-circle" viewBox="0 0 16 16">
-                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                            <path fill-rule="evenodd"
-                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                        </svg> -->
                         <h3 v-if="userDetails !== null">{{  userDetails.username  }}</h3>
                     </div>
                     <div class='profileEachRow'>
@@ -19,7 +13,7 @@
                             <a class="profileLoader" v-if="userDetails === null">
                                 <pulse-loader :loading="loading" :color="color" :size="size"></pulse-loader>
                             </a>
-                            <a v-if="userDetails !== null">
+                            <a v-if="userDetails !== null" class="userDetailContainer">
                                 <div v-if="userDetails.gender !== null && userDetails.gender !== ''">
                                     <svg style="margin-top: -5px; margin-left: -4px;" xmlns="http://www.w3.org/2000/svg"
                                         width="40" height="40" viewBox="0 0 24 24" fill="white">
@@ -243,12 +237,6 @@
         <div class="profileMostViewedContainer profileMobileMostViewedContainer">
             <div class="profileMostViewedPages profileMobileContainer">
                 <div style="display: flex;" class="profileUsernameContainer">
-                    <!-- <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black"
-                        class="bi bi-person-circle" viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                        <path fill-rule="evenodd"
-                            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                    </svg> -->
                     <h3 v-if="userDetails !== null">{{  userDetails.username  }}</h3>
                 </div>
                 <div class='profileEachRow'>
@@ -258,7 +246,7 @@
                         <a class="profileLoader" v-if="userDetails === null">
                             <pulse-loader :loading="loading" :color="color" :size="size"></pulse-loader>
                         </a>
-                        <a v-if="userDetails !== null">
+                        <a v-if="userDetails !== null" class="userDetailContainer">
                             <div v-if="userDetails.gender !== null && userDetails.gender !== ''">
                                 <svg style="margin-top: -5px; margin-left: -4px;" xmlns="http://www.w3.org/2000/svg"
                                     width="40" height="40" viewBox="0 0 24 24" fill="white">
