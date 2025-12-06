6c07eaa9468400b266256d20c7f6c4721c44689d
Heesu Kim <nikolaikim27@gmail.com>
2025-12-06 13:55:23 +0900

    update google client id

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import vue3GoogleLogin from 'vue3-google-login'
 
 const app = createApp(App)
 
-app.use(vue3GoogleLogin, { clientId: '588194928402-46vdh77fbp1ah45srvdajluvqcbhcpee.apps.googleusercontent.com' })
+app.use(vue3GoogleLogin, { clientId: '613954349182-vl2rcqdm42p5sr3gsd2li7tb7eki0skk.apps.googleusercontent.com' })
 app.use(VueCookies)
 app.use(router)
 app.mount('#app')
